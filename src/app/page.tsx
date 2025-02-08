79e7b1dacfc12d5f9a1fb59da7b605216ae120fc
Benny-Nwaro <baroh3767@aol.com>
2025-02-09 00:05:06 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 // src/pages/index.js
-import Dashboard from '../components/Dashboard';
+import Dashboard from '@/components/Dashboard';
 
 export default function Home() {
-  return <h1>Welcome to My App!</h1>;
+  return <Dashboard/>;
   
 }
